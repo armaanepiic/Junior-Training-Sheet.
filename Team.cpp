c10391ee71d789f8b4eb9ece68e518a50e4b1388
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2021-10-20 02:03:23 +0600

    Add files via upload

---
+++ Team.cpp
@@ -0,0 +1,25 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pi 3.14159
+#define pb push_back
+void input_output()
+{
+    /* #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif */
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main()
+{
+    input_output();
+
+        return 0;
+}
